d71af729d88ea5ed6dd9875ab6260403caeb3542
kay <fleischmann.kay@googlemail.com>
2014-06-24 20:23:31 +0200

    clenaup

--- blos-examples/src/main/java/eu/blos/java/array/TestArraySizes.java
+++ blos-examples/src/main/java/eu/blos/java/array/TestArraySizes.java
@@ -7,26 +7,9 @@ import pl.edu.icm.jlargearrays.FloatLargeArray;
 public class TestArraySizes {
     public static void main(String[] args) throws Exception {
         System.out.println("Test Memory");
-        int length=500;
-
-        //byte[] buffer = new byte[size*1024*1024];
-        //int[] buffer = new int[size*1024*1024];
-        //byte[][] count = new byte[(length*(1024*1024)/1)][1];
-        //byte[][] count = new byte [12][(length*(1024*1024)/1)];
-
         PDDCMSketch pdd1 = new PDDCMSketch(0.01, 0.00000001);
 
         pdd1.alloc();
-        //System.out.println( pdd1.memory_size() / 1024.0 / 1024.0 );
-        //PDDCMSketch pdd2 = new PDDCMSketch(0.1, 0.1);
-        //PDDSet set = new PDDSet(pdd1);
-        //set.alloc();
-        /*
-        long w = 1684354560L;
-        long d = 1;
-
-        FloatLargeArray a = new FloatLargeArray(w*d);*/
-
 
         pdd1.update("hallo", 1 );
         pdd1.update("hallo", 1 );
