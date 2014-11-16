aa0b3c00e2a7aeb518217313a9780c18e4da5c0f
kay <fleischmann.kay@googlemail.com>
2014-11-16 01:24:30 +0100

    dataset generator for multi-parameter regression

---
+++ blos-examples/src/main/python/eu/blos/datasets/reg.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+################################################################################
+#
+# Authors:       Fleischmann, Kay (fleischmann.kay@googlemail.com)
+# Documentation: -
+#
+################################### LICENSE ####################################
+#
+# Copyright (C) 2014 Fleischmann, Kay
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+################################ DOCUMENTATION #################################
+#
+# see output (-h)
+#
+#
+#
+################################################################################
+
+
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+#zip_with = lambda fn, la, lb: [fn(a, b) for (a, b) in zip(la, lb)]
+
+import numpy as np
+import sys
+def run(**kwargs):
+	sigma = float(kwargs['sigma'])
+	samples = int(kwargs['count'])
+	f = kwargs['function'].split(":")
+	sep=","
+	range_start=float(kwargs['range'].split(":")[0])
+	range_end=float(kwargs['range'].split(":")[1])
+
+	def reg_func(x):
+		sum=0
+		for i, fk in enumerate(f):
+			sum+=i
+		return sum
+
+	x = 0
+	counter = 0
+	for s in range(0, samples):
+		x = np.random.uniform(range_start,range_end)
+		y = reg_func(x) + np.random.uniform(-1,1)*sigma
+		print str(counter)+sep+str(x)+sep+str(y)
+		counter += 1
+
+if __name__=='__main__':
+	# handle command line arguments
+	# http://docs.python.org/2/library/argparse.html
+	import argparse
+	from argparse import RawTextHelpFormatter
+
+	argparser = argparse.ArgumentParser(description="""
+		create linear regression datasets
+	""", formatter_class=RawTextHelpFormatter)
+
+	# specify arguments
+	argparser.add_argument("-v", "--verbose", action="store_true", help="turns on verbosity")
+	argparser.add_argument("-s", "--sigma", action="store", required=True, help="variance")
+	argparser.add_argument("-c", "--count", action="store", required=True, help="number of samples")
+	argparser.add_argument("-f", "--function", action="store", required=True, help="h1:h2:h3...")
+	argparser.add_argument("-r", "--range", action="store", required=True, help="x1:x2")
+
+	# validate arguments
+	args = argparser.parse_args()
+
+	try:
+		# execute main script and delegate command line options
+		run(**vars(args))
+
+	except Exception as e:
+		print "AN ERROR OCCOURED:", e
+
+		if( bool(vars(args)['verbose']) ):
+			import traceback, sys
+			traceback.print_exc(file=sys.stderr)
---
+++ blos-examples/src/main/scala/eu/blos/scala/ml/gradient_decent/TestGradientDecentLinReg.scala
@@ -0,0 +1,134 @@
+package eu.blos.scala.ml.gradient_decent
+
+
+/**
+ * This class loads a dataset into an count-min sketch and
+ * applies and linear-regresion with gradient-decent using
+ * the count min sketch
+ *
+ *
+ * this should very the mechanics of an gradient-decent with
+ * an underlying count-min sketch
+ */
+
+import scala.io.Source
+import eu.blos.scala.algorithms.sketches.CMSketch
+
+
+// sample_1 - sampleId
+// sample_2 - X-value
+// sample_3 - Y-value
+
+class TestGradientDecentLinReg( val alpha : Double, val dataset : Array[(Int,List[Double],Double)] ) {
+  var model : Array[Double] = new Array[Double](2)
+  def getY( i : Integer ) ={
+    dataset(i)._3
+  }
+
+  def getX( i: Integer, j : Integer ) = {
+    dataset(i)._2(j)
+  }
+  def getX( i: Integer ) = {
+    dataset(i)._2
+  }
+
+  def getXY( i : Integer, k : Integer ) = {
+    getY(i) * getX(i, k)
+  }
+
+  def getXX( i : Integer, j : Integer, k : Integer ) = {
+    getX(i,j)*getX(i, k)
+  }
+
+
+  def gradient_decent( model : Array[Double], dataset :Array[(Int,List[Double],Double)] ) = {
+    val new_model = new Array[Double](2)
+
+    var errors_x1=0.0
+    var errors_x2=0.0
+
+    for( i <- Range(0,dataset.length)){
+
+      errors_x1 += - getXY(i, 0) //getY(i) * getX(i, 0)
+      errors_x2 += - getXY(i, 1) //getY(i) * getX(i, 1)
+
+      for( j <- Range(0,model.length)) {
+        errors_x1 += model(j)*getXX(i, j, 0)  //getXX(i,j)*getX(i, 0)
+        errors_x2 += model(j)*getXX(i, j, 1)  //getXX(i,j)*getX(i, 1)
+      }
+
+    }
+    model(0)  = model(0) - alpha * (1.0/dataset.length) * errors_x1
+    model(1)  = model(1) - alpha * (1.0/dataset.length) * errors_x2
+
+    model
+  }
+
+  def run = {
+    // y = m*x +b
+    var model = new Array[Double](2)
+    model(0) = 0  // b
+    model(1) = 0  // m
+
+    for(x <- Range(0,30000)) {
+      model = gradient_decent(model, dataset)
+      println( model(0)+","+model(1) )
+    }//for
+  }
+}
+
+
+class TestGradientDecentLinRegWithSketch( override val alpha : Double, override val dataset : Array[(Int,List[Double],Double)] )
+  extends TestGradientDecentLinReg(alpha, dataset) {
+
+  val sketch_labels = new CMSketch(0.1 /*factor*/, 0.0001 /*prob*/);
+  val sketch_samples = new CMSketch(0.1 /*factor*/, 0.00001 /*prob*/);
+
+  sketch_labels.alloc
+  sketch_samples.alloc
+
+  println(sketch_labels.size)
+
+  sketch
+
+  def sketch = {
+    println("sketching")
+
+    for( i <- Range(0,dataset.length)) {
+      sketch_labels.update( i+"-"+0, (getY(i)*getX(i,0)).toFloat )
+      sketch_labels.update( i+"-"+1, (getY(i)*getX(i,1)).toFloat )
+
+      for( j <- Range(0,model.length)) {
+        sketch_samples.update( i+"-"+j+"-"+0, (getX(i,0)*getX(i,1)).toFloat )
+        sketch_samples.update( i+"-"+j+"-"+1, (getX(i,1)*getX(i,0)).toFloat )
+      }
+    }
+  }
+
+  override def getXY( i : Integer, k : Integer ) = {
+    // getY(i) * getX(i, k)
+    sketch_labels.get(i+"-"+k)
+  }
+
+  override def getXX( i : Integer, j : Integer, k : Integer ) = {
+    //getX(i,j)*getX(i, k)
+    sketch_samples.get(i+"-"+j+"-"+k)
+  }
+
+
+}
+
+object TestGradientDecentLinReg extends Serializable {
+  def main(args:Array[String]){
+    val filename = args(0)
+    val dataset = Source.fromFile(filename).getLines().map( x => {
+      val fields = x.split(",")
+      //(SampleId, X-Values, Y
+      (fields(0).toInt, List(1.0) ++ fields(1).split(" ").map(x=>x.toDouble), fields(2).toDouble )
+    }).toArray
+    println("dataset loaded")
+
+
+    new TestGradientDecentLinRegWithSketch(0.000005, dataset).run
+  }
+}
